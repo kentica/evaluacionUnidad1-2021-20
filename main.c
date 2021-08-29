5e07f7e3e161efd76499f791ecdc8417ab0f7dd9
sebastian r <sebastian.ramirezar@upb.edu.co>
2021-08-28 20:25:37 -0500

    Implementando el feature 1
    No se usar git kraken xd

--- main.c
+++ main.c
@@ -4,5 +4,21 @@
 
 int main(int argc, char *argv[]){
 
-    return EXIT_SUCCESS;
+    FILE *inFile = fopen("./fin","r");
+    FILE *inFileOut = fopen("./fout","a+");
+    if(inFile == NULL){
+        perror("Open File fin Fails :");
+        return(EXIT_FAILURE);
+    }
+    if(inFileOut == NULL){
+        perror("Open File fout fails: ");
+        return(EXIT_FAILURE);
+    }
+    for(int i=0;i<6;i++){
+        if(i ==1 ) feature1(inFile,inFileOut);
+    }
+    
+    fclose(inFile);
+    fclose(inFileOut);
 }
+

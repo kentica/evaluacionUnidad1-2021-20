8c30b2df35e1735d86e3240daeebf3a58fe42e26
sebastian r <sebastian.ramirezar@upb.edu.co>
2021-08-28 20:25:37 -0500

    Implementando el feature 1
    No se usar git kraken xd

--- student.c
+++ student.c
@@ -1,7 +1,18 @@
 #include <stdlib.h>
 #include "student.h"
+#include <string.h>
 
 void feature1(FILE *fin, FILE *fout){
-
-    
+    char buffer[64];
+    char *status = NULL;
+    int cont= 0; //Para controlar el ciclo que se haga solo para leer la primera linea de el archivo
+    do{
+        status = fgets(buffer,sizeof(buffer),fin);
+        cont++;
+        if(status != NULL){
+        fputs(buffer, fout);
+        }               
+    }
+    while (cont<=0);
+    printf("\n");   
 }

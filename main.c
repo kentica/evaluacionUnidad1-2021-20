3027aed5dfde3af72c844b1112f6c912dfc57052
sebastian r <sebastian.ramirezar@upb.edu.co>
2021-08-29 20:25:14 -0500

    Feature 4, o al menos deberia de serlo pero no funciona :C
    Quiero llorar, muerte a c, al menos aprendi a usar git xd

--- main.c
+++ main.c
@@ -2,10 +2,21 @@
 #include <stdlib.h>
 #include "student.h"
 
+int *create_array(int);
+char *create_String(int);
+void destroy_array(int *);
+void destroy_String(char *);
+
 int main(int argc, char *argv[]){
 
-    FILE *inFile = fopen("./fin","r");
-    FILE *inFileOut = fopen("./fout","a+");
+    FILE *inFile = fopen("./fin", "r");
+    FILE *inFileOut = fopen("./fout", "a+");
+    char *auxop = create_String(10);
+    char **op = &auxop;
+    int *auxparr = create_array(20);
+    int **parr = &auxparr;
+    int *length;
+
     if(inFile == NULL){
         perror("Open File fin Fails :");
         return(EXIT_FAILURE);
@@ -14,17 +25,34 @@ int main(int argc, char *argv[]){
         perror("Open File fout fails: ");
         return(EXIT_FAILURE);
     }
-<<<<<<< HEAD
     for(int i=0;i<8;i++){
         if(i ==1 ) feature1(inFile,inFileOut);
         if(i ==2 ) feature2(inFile,inFileOut);
-=======
-    for(int i=0;i<6;i++){
-        if(i ==1 ) feature1(inFile,inFileOut);
->>>>>>> main
+        if(i ==3 ) feature3(inFile,inFileOut);
+        //if(i ==4 ) feature4(inFile, parr, length, op);                
+        //if(i == 7) feature7(inFileOut,&pobj);
     }
+        destroy_array(auxparr);
+    destroy_String(auxop);
     
     fclose(inFile);
     fclose(inFileOut);
+    
+    return EXIT_SUCCESS;
 }
 
+int *create_array(int size){
+    return (int * ) malloc(size * sizeof(int));
+}
+
+char *create_String(int size) {
+    return (char * ) malloc(size  * sizeof(char));
+}
+
+void destroy_array(int *this){
+    free(this);
+}
+
+void destroy_String(char *this){
+    free(this);
+}
--- student.c
+++ student.c
@@ -2,10 +2,11 @@
 #include "student.h"
 #include <string.h>
 
+
 void feature1(FILE *fin, FILE *fout){
-    char buffer[64];
+    char buffer[128];
     char *status = NULL;
-    int cont= 0; //Para controlar el ciclo que se haga solo para leer la primera linea de el archivo
+    int cont= 0; 
     do{
         status = fgets(buffer,sizeof(buffer),fin);
         cont++;
@@ -15,12 +16,11 @@ void feature1(FILE *fin, FILE *fout){
     }
     while (cont<=0);
     printf("\n");   
-<<<<<<< HEAD
 }
 
 void feature2(FILE *fin, FILE *fout) {
     
-    char buffer[70];
+    char buffer[128];
     char *status = NULL;
 
     status = fgets(buffer, sizeof(buffer), fin);
@@ -31,6 +31,50 @@ void feature2(FILE *fin, FILE *fout) {
     }
     printf("\n");
 }
-=======
+
+void feature3(FILE *fin, FILE *fout)
+{
+    int contador = 0;
+    char buffer[10];
+    int resultado = 0, valoractual = 0;
+    char *estado = NULL;
+    estado = fgets(buffer, sizeof(buffer), fin);
+    if (estado != NULL)
+    {
+        for (contador = 0; contador <= sizeof(buffer); contador++)
+        {   fscanf(fin , "%d",&valoractual);
+            resultado = resultado + valoractual; 
+        }
+     }
+    
+    fprintf(fout, "%d", resultado);
 }
->>>>>>> main
+/*void feature4(FILE *fin, int **parr, int *length, char **op){
+    char buffer[250];
+    char cadtemp = "";
+    int sum = 0;
+    int cant = 0;
+    int avg = 0;
+    char *status =  NULL;
+    int i;
+
+    if (fgets(buffer, 250, buffer) != NULL)
+    {
+            for (i = 0; i >= sizeof(buffer) ; i++)
+            {
+                if (isdigit(buffer[i])){
+                    strcpy(cadtemp, buffer[i]);
+                }
+                else{
+                    sum = sum + atoi(cadtemp);
+                    cant++;
+                    if (buffer[i] == "a"){
+                        avg = sum/cant;
+                        printf("avg is %d", avg);
+                    }
+                }
+            }
+    }
+    return(EXIT_SUCCESS);
+}
+*/
